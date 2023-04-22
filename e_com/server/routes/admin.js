51830991faca2101d5fce6df2596c16352248584
Harsh Gupta <imguptaharsh25@gmail.com>
2023-04-22 18:25:49 +0530

    custom changes in admin portal

--- e_com/server/routes/admin.js
+++ e_com/server/routes/admin.js
@@ -64,7 +64,55 @@ adminRouter.get('/admin/get-products', admin,async(req,res)=>{
       res.json(order);
     } catch(e){
       res.status(500).json({error: e.message});
+    }  
+  });
+
+  adminRouter.get('/admin/analytics',admin, async(req,res)=>{
+    try {
+      const orders = await Order.find({});
+      let totalEarnings = 0;
+  
+      for (let i = 0; i < orders.length; i++) {
+        for (let j = 0; j < orders[i].products.length; j++) {
+          totalEarnings +=
+            orders[i].products[j].quantity * orders[i].products[j].product.price;
+        }
+      }
+      // CATEGORY WISE ORDER FETCHING
+    let menEarnings = await fetchCategoryWiseProduct("Men");
+    let womemEarnings = await fetchCategoryWiseProduct("Women");
+    let kidsEarnings = await fetchCategoryWiseProduct("kids");
+    let shoesEarnings = await fetchCategoryWiseProduct("Shoes");
+    // let fashionEarnings = await fetchCategoryWiseProduct("Fashion");
+
+    let earnings = {
+      totalEarnings,
+      menEarnings,
+      womemEarnings,
+      kidsEarnings,
+      shoesEarnings,
+      // fashionEarnings,
+    };
+
+    res.json(earnings);
+    } catch(e){
+      res.status(500).json({error: e.message});
     }
   });
 
+  async function fetchCategoryWiseProduct(category) {
+    let earnings = 0;
+    let categoryOrders = await Order.find({
+      "products.product.category": category,
+    });
+  
+    for (let i = 0; i < categoryOrders.length; i++) {
+      for (let j = 0; j < categoryOrders[i].products.length; j++) {
+        earnings +=
+          categoryOrders[i].products[j].quantity *
+          categoryOrders[i].products[j].product.price;
+      }
+    }
+    return earnings;
+  }
 module.exports=adminRouter;
